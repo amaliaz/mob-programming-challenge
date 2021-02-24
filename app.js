3fa4b87f50df4abebc2960deb2a0267970c3ab7a
Aurelie75FR <aurelielo.vi@gmail.com>
2021-02-24 10:43:46 +0100

    iteration 5 done

--- app.js
+++ app.js
@@ -1,2 +1,21 @@
 require("dotenv").config();
-require("./config/mongodb.js")
+require("./config/mongodb.js");
+
+const express = require("express");
+const app = express();
+const hbs = require("hbs");
+const { set } = require("mongoose");
+
+app.use(express.static(__dirname + "/public"));
+app.set("views", __dirname + "/views");
+app.set("view engine", "hbs");
+
+const user = [
+  { name: "toto", email: "toto@iron.com", favoriteLanguage: "Javascript" },
+  { name: "lolo", email: "lolo@iron.com", favoriteLanguage: "Javascript" },
+  { name: "titi", email: "titi@iron.com", favoriteLanguage: "Javascript" },
+];
+
+const images = ["/img/one.jpeg", "/img/two.jpeg", "/img/three.jpeg"];
+
+app.listen(process.env.PORT,()=>{console.log(`welcome @http://localhost:${process.env.PORT}`)})
